bd670ee4e06330d6c70756246d4128faa2afe6c7
Mohamed Amine Frioua <aminefrioua@gmail.com>
2022-12-28 17:37:50 +0900

    add link to degree

--- .formatter.exs
+++ .formatter.exs
@@ -1,6 +1,12 @@
 [
   import_deps: [:phoenix],
-  inputs: ["*.{ex,exs}", "config/**/*.{ex,exs}"],
+  inputs: [
+    "*.{ex,exs}",
+    "config/**/*.{heex,ex,exs}",
+    "*.{heex,ex,exs}",
+    "{lib,test}/**/*.{heex,ex,exs}"
+  ],
   plugins: [Phoenix.LiveView.HTMLFormatter],
-  subdirectories: ["apps/*"]
+  subdirectories: ["apps/*"],
+  heex_line_length: 120
 ]
--- lib/portfolio_web/views/page_view.ex
+++ lib/portfolio_web/views/page_view.ex
@@ -1,3 +1,7 @@
 defmodule PortfolioWeb.PageView do
   use PortfolioWeb, :view
+
+  def certificate_link() do
+    "https://certificate.bcdiploma.com/check/CBB76D3A8B4AD4814B90447F66E2283A67898DBEC2318BE90021D929FAFBE65BL1lZYzFuMXdONkpRVWVyaWs3STVaaEJVempSdFArTEhpWjV5eEo5eVFLZUp3em1M"
+  end
 end
